8a803deaab943cfb43c21552d838f6cee8a3884b
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-07-15 20:14:16 +0200

    prepared navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import {
     AppBar,
     Button,
+    Link,
     createStyles,
-    IconButton,
     makeStyles,
     Theme,
     Toolbar,
     Typography
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu'
+import TheaterIcon from '@material-ui/icons/Theaters';
 import React from 'react';
-import './App.css';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
         },
-        menuButton: {
-            marginRight: theme.spacing(2),
+        theaterIcon: {
+            fontSize: "2rem",
+            paddingRight: "5px"
         },
         title: {
             flexGrow: 1,
@@ -32,15 +32,19 @@ function App() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon/>
-                </IconButton>
+                <TheaterIcon className={classes.theaterIcon}/>
                 <Typography variant="h6" className={classes.title}>
                     Movie Database
                 </Typography>
-                <Button color="inherit">Search</Button>
-                <Button color="inherit">Detail</Button>
-                <Button color="inherit">Favorites</Button>
+                <Link href="search" color="inherit" underline="none">
+                    <Button color="inherit">Search</Button>
+                </Link>
+                <Link href="detail" color="inherit" underline="none">
+                    <Button color="inherit">Detail</Button>
+                </Link>
+                <Link href="favorites" color="inherit" underline="none">
+                    <Button color="inherit">Favorites</Button>
+                </Link>
             </Toolbar>
         </AppBar>
     );
