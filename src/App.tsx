9109b5f389e10c90f732533ad81901203e23301d
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-07-16 14:13:16 +0200

    created form with search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Header from "./Header";
+import {createStyles, makeStyles, TextField, Theme} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        form: {
+            margin: "auto",
+            width: "fit-content",
+            marginTop: "2em"
+        },
+        searchInput: {
+            width: 500
+        }
+    })
+);
+
 
 function App() {
 
+    const classes = useStyles();
+
     return (
+        <>
         <Header />
+        <form className={classes.form} noValidate autoComplete="off">
+            <TextField id="outlined-basic" className={classes.searchInput} label="Search" variant="outlined" />
+        </form>
+        </>
     );
 }
 
