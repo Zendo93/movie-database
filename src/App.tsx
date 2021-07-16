1d9abcdbbc862d356ad4f2c96be8386a3dc9f77b
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-07-16 13:10:57 +0200

    extracted code to Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,10 @@
-import {
-    AppBar,
-    Button,
-    Link,
-    createStyles,
-    makeStyles,
-    Theme,
-    Toolbar,
-    Typography
-} from '@material-ui/core';
-import TheaterIcon from '@material-ui/icons/Theaters';
 import React from 'react';
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-        },
-        theaterIcon: {
-            fontSize: "2rem",
-            paddingRight: "5px"
-        },
-        title: {
-            flexGrow: 1,
-        },
-    }),
-);
+import Header from "./Header";
 
 function App() {
-    const classes = useStyles();
 
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <TheaterIcon className={classes.theaterIcon}/>
-                <Typography variant="h6" className={classes.title}>
-                    Movie Database
-                </Typography>
-                <Link href="search" color="inherit" underline="none">
-                    <Button color="inherit">Search</Button>
-                </Link>
-                <Link href="detail" color="inherit" underline="none">
-                    <Button color="inherit">Detail</Button>
-                </Link>
-                <Link href="favorites" color="inherit" underline="none">
-                    <Button color="inherit">Favorites</Button>
-                </Link>
-            </Toolbar>
-        </AppBar>
+        <Header />
     );
 }
 
