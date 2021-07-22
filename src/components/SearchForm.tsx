127ec9d5fcc69342cd09c12e91401fdaeec84be4
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-07-22 18:23:55 +0200

    use useState hook in SearchForm.tsx

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,5 @@
 import {Button, createStyles, makeStyles, TextField, Theme} from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,10 +20,12 @@ const useStyles = makeStyles((theme: Theme) =>
 function SearchForm() {
 
     const classes = useStyles();
+    const [searchTerm, setSearchTerm] = useState("");
 
     return (
         <form className={classes.form} noValidate autoComplete="off">
-            <TextField id="outlined-basic" className={classes.searchInput} label="Search" variant="outlined" />
+            <TextField id="outlined-basic" className={classes.searchInput} label="Search" variant="outlined"
+                       onChange={event => setSearchTerm(event.target.value)} value={searchTerm} />
             <Button className={classes.submitButton} variant="outlined">Submit</Button>
         </form>
     );
